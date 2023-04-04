c9a4dad2139ac3befea7cfbbf59f89713b24a41c
sabrinaest <estrasb@oregonstate.edu>
2023-04-04 00:28:09 -0700

    wasn't working because my files were png and the code uses jpg

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import time
 
 while True:
-    user_input = input("Please enter 1 to generate an image. Enter 2 if you wish to exit: ")
+    user_input = input("Please enter 1 to meet your new Animal Crossing neighbor! Enter 2 if you wish to exit: ")
     if user_input == '1':
         with open('prng-service.txt', 'w') as prng_file:
             prng_file.write('run')
@@ -15,14 +15,26 @@ while True:
             img_service_file.seek(0)
             img_service_file.truncate()
             img_service_file.write(random_num)
-
+        print('.')
+        print('.')
+        print('.')
+        print('Looks like they are preparing a house warming basket! :)')
         time.sleep(5)
 
         with open('image-service.txt', 'r') as img_service_file:
             img_path = img_service_file.readline()
-            print('Here is your image:', img_path)
+            print('.')
+            print('.')
+            print('.')
+            print('They are very excited to meet you!')
+            time.sleep(5)
+            print('.')
+            print('.')
+            print('.')
+            print('Meet your new neighbor:', img_path)
 
     elif user_input == '2':
+        print('Goodbye!')
         break
     else:
         print("unknown option")
